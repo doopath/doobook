97988559c0fa1083bf8f770d8c31afa8d92a5bbe
sha1om <maminpahat@gmail.com>
2020-06-07 20:19:09 +0300

    Added give-feedback page

--- templates/dist/js/script.js
+++ templates/dist/js/script.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
     //Show full image on review-page
+
+    //set proportion for review__image__container
     let image_height = $('body').width() / (4 / 3);
 
+    //set new dynamic height for review__image__container and show/hide "View more" button
     if ($('.review__image').height() > image_height) {
         $('.view-full-image').css('display', 'block');
     } else {
@@ -9,6 +12,7 @@ $(document).ready(function() {
         $('.review__image__container').css('border-radius', '5px');
     }
 
+    //replace the text for "View more" button as "Hide picture"
     $('.view-full-image').on('click', function(event) {
         if ($('.view-full-image p').text() === 'Show full image') {
             $('.view-full-image p').text('Hide image');
@@ -16,10 +20,19 @@ $(document).ready(function() {
             $('.view-full-image p').text('Show full image');
         }
 
+        //show full-size image when had been clicked the "View more" button
         $('.review__image__container').toggleClass('full-image');
     });
 
     $('.transform-block').on('click', function(event) {
         $('.transform-block').css('width', '100%');
+    });
+
+    $('.key__image').change(function() {
+        if ($(this).val() != '') {
+            $('.key__image__label').text("Chose one image");
+        } else {
+            $('.key__image__label').text("Choose an image");
+        }
     });
 });
